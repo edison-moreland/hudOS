7cd15631a4fb5fd4bc9c21c05ff6ad041d579413
Edison Moreland <edison@devdude.net>
2023-02-14 20:00:40 -0800

    Almost

--- buildroot/build.sh
+++ buildroot/build.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname $(realpath $0))"
 source "${SCRIPT_DIR}"/../logging.sh
 
+if [[ $EUID -eq 0 ]]
+then
+  log_red "This script should NOT be ran as root!"
+  exit 1
+fi
+
 BUILDROOT_DIR="${SCRIPT_DIR}/.buildroot"
 
 if [ ! -d "${BUILDROOT_DIR}" ]
--- buildroot/image.sh
+++ buildroot/image.sh
@@ -5,8 +5,10 @@ SCRIPT_DIR="$(dirname $(realpath $0))"
 source "${SCRIPT_DIR}"/../logging.sh
 
 REALUSER="${USER}"
-if [[ $EUID -eq 0 ]]; then
-  if [[ -n "$SUDO_USER" ]]; then
+if [[ $EUID -eq 0 ]]
+then
+  if [[ -n "$SUDO_USER" ]]
+  then
     REALUSER="${SUDO_USER}"
   fi
 else
@@ -30,7 +32,7 @@ then
 fi
 
 OUTPUT_IMAGE="${SCRIPT_DIR}/hudOS.img"
-OUTPUT_IMAGE_MB="3072"
+OUTPUT_IMAGE_MB="500"
 
 if [ -f "${OUTPUT_IMAGE}" ]
 then
@@ -40,37 +42,39 @@ fi
 
 log_blue "Reseving space for new image"
 dd if=/dev/zero of="${OUTPUT_IMAGE}" bs=1M count=0 seek="${OUTPUT_IMAGE_MB}"
+sync
 LODEV=$(losetup --find)
 losetup "${LODEV}" "${OUTPUT_IMAGE}"
 function clean_lodev {
+  log_yellow "Cleaning lodev"
   losetup -d "${LODEV}"
 }
 trap clean_lodev EXIT
 
 log_blue "Partitioning..."
-parted -s "${LODEV}" mklabel msdos
+parted -s "${LODEV}" mklabel gpt
 parted -s "${LODEV}" unit s mkpart primary 8192 491519
 parted -s "${LODEV}" unit s mkpart primary 524288 100%
 
 log_blue "Formatting boot partition"
-mkfs.vfat -n BOOT "${LODEV}p1"
+mkfs.vfat -n BOOT "${LODEV}p1"-
 
 log_blue "Copying rootfs partition to image"
 pv < "${BUILDROOT_OUTPUT_DIR}/rootfs.ext4" > "${LODEV}p2"
 
 MNT_ROOT="$(mktemp -d)"
 mount "${LODEV}p2" "${MNT_ROOT}"
-mkdir "${MNT_ROOT}/boot"
-mount "${LODEV}p1" "${MNT_ROOT}/boot"
 function clean_mnt {
-  umount "${MNT_ROOT}/boot"
-  rm -r "${MNT_ROOT:?}/boot"
+  log_yellow "Cleaning mount"
   umount "${MNT_ROOT}"
+  rm -r "${MNT_ROOT}"
+  clean_lodev
 }
 trap clean_mnt EXIT
 
+KERNEL_IMAGE="${BUILDROOT_OUTPUT_DIR}/Image.gz"
+INITRAMFS="${BUILDROOT_OUTPUT_DIR}/rootfs.cpio"
 
-ls -la "${MNT_ROOT}"
 
 # Fix fstab
 # Copy boot files
