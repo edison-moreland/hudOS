b916db4eb7d7f5fe1545aab2983a658332416a44
Edison Moreland <edison@devdude.net>
2023-02-16 19:06:01 -0800

    Fix tty1; Autologin hud user

--- buildroot/br2_hudos/package/hud-landing-pad/hud-landing-pad.mk
+++ buildroot/br2_hudos/package/hud-landing-pad/hud-landing-pad.mk
@@ -3,16 +3,13 @@ HUD_LANDING_PAD_SITE = $(BR2_EXTERNAL_HUDOS_PATH)/package/hud-landing-pad
 HUD_LANDING_PAD_SITE_METHOD = local
 PINE_FIRMWARE_INSTALL_TARGET = YES
 
+# TODO(edison):
 define HUD_LANDING_PAD_USERS
-	hud -1 hud -1 * /opt/hud - - hudOS
-	deploy -1 deploy -1 =changechangethis /home/deploy /bin/bash wheel,sudo hudOS Deploy
+	hud -1 hud -1 * /opt/hud /bin/bash - hudOS
+	deploy -1 deploy -1 - /home/deploy /bin/bash wheel,sudo hudOS Deploy
 endef
 
-# TODO
-# Add depends for wpa_supplicant, and systemd-networkd
-# Created symlink /etc/systemd/system/multi-user.target.wants/wpa_supplicant@wlan0.service → /usr/lib/systemd/system/wpa_supplicant@.service
-
-define HUD_LANDING_PAD_INSTALL_TARGET_CMDS
+define HUD_LANDING_PAD_SETUP_WIFI
 	if [ "$(BR2_PACKAGE_HUD_LANDING_PAD_WIFI_SSID)" == "" ]; then \
 		echo "Please use 'build.sh nconfig' to set wifi settings under External Settings"; \
 		exit 1; \
@@ -27,6 +24,17 @@ define HUD_LANDING_PAD_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -D -m 644 $(HUD_LANDING_PAD_PKGDIR)/20-wifi.preset \
 		$(TARGET_DIR)/usr/lib/systemd/system-preset/20-wifi.preset;
+endef
+
+define HUD_LANDING_PAD_SETUP_AUTOLOGIN
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/getty@tty1.service.d/; \
+	$(INSTALL) -D -m 644 $(HUD_LANDING_PAD_PKGDIR)/autologin.conf \
+		$(TARGET_DIR)/etc/systemd/system/getty@tty1.service.d/autologin.conf;
+endef
+
+define HUD_LANDING_PAD_INSTALL_TARGET_CMDS
+	$(HUD_LANDING_PAD_SETUP_WIFI)
+	$(HUD_LANDING_PAD_SETUP_AUTOLOGIN)
 endef
 
 $(eval $(generic-package))
