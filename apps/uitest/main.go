31cc9fe74653c3f158909706d4c6a92eca4eba41
Edison Moreland <edison@devdude.net>
2023-02-21 16:45:43 -0800

    Replace uitest with a less complex example from Neurlang's repo

--- apps/uitest/main.go
+++ apps/uitest/main.go
@@ -18,524 +18,352 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-// Go Wayland Smoke demo
+// Go Wayland SimpleShm demo
 package main
 
-import "time"
-import "math/rand"
-
-import cairo "github.com/neurlang/wayland/cairoshim"
 import "github.com/neurlang/wayland/wl"
-import "github.com/neurlang/wayland/window"
-import "fmt"
+import zxdg "github.com/neurlang/wayland/xdg"
+import "github.com/neurlang/wayland/wlclient"
+import "log"
+import "github.com/neurlang/wayland/os"
 
-type smoke struct {
-	display     *window.Display
-	window      *window.Window
-	widget      *window.Widget
-	width       int32
-	height      int32
-	smallwidth  int32
-	smallheight int32
-	o           []float32
-	bb          [2]struct {
-		d  []float32
-		uv chan [2][]float32
-		uu chan []float32
-		vv chan []float32
-	}
-	pipe   bool
-	lx, ly float32
+type display struct {
+	display    *wl.Display
+	registry   *wl.Registry
+	compositor *wl.Compositor
+	shell      *zxdg.WmBase
+	shm        *wl.Shm
+	hasXrgb    bool
+}
+type buffer struct {
+	buffer  *wl.Buffer
+	shmData []byte
+	busy    bool
 }
 
-func diffuse(smoke *smoke, time uint32, source []float32, dest []float32, width int32, height int32) {
-	var s, d []float32
-	var x, y, k, stride int32
-	var t float32
-	var a float32 = 0.0002
+type window struct {
+	display          *display
+	width, height    int
+	surface          *wl.Surface
+	xdgSurface       *zxdg.Surface
+	xdgToplevel      *zxdg.Toplevel
+	buffers          [2]buffer
+	callback         *wl.Callback
+	waitForConfigure bool
+}
 
-	stride = width
-
-	for k = 0; k < 5; k++ {
-		for y = 1; y < height-1; y++ {
-			s = source[y*stride:]
-			d = dest[y*stride-stride:]
-			for x = 1; x < width-1; x++ {
-				t = d[x-1+stride] + d[x+1+stride] +
-					d[x] + d[x+stride+stride]
-				d[x+stride] = (s[x] + a*t) / (1 + 4*a) * 0.995
-			}
-		}
+func handle(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func advect(
-	smoke *smoke,
-	time uint32,
-	uu []float32,
-	vv []float32,
-	source []float32,
-	dest []float32,
-	width int32, height int32,
-) {
+func (mybuf *buffer) HandleBufferRelease(wl.BufferReleaseEvent) {
+	mybuf.busy = false
+}
 
-	var s, d []float32
-	var u, v []float32
-	var x, y, stride int
-	var i, j int
-	var px, py, fx, fy float32
+func createShmBuffer(w *window, buffer *buffer, width int, height int, format uint32) error {
 
-	stride = int(width)
+	stride := width * 4
+	var size = stride * height
 
-	for y = 1; y < int(height)-1; y++ {
-		d = dest[y*stride:]
-		u = uu[y*stride:]
-		v = vv[y*stride:]
+	fd, err := os.CreateAnonymousFile(int64(size))
+	if err != nil {
+		return err
+	}
 
-		for x = 1; x < int(width)-1; x++ {
-			px = float32(x) - u[x]
-			py = float32(y) - v[x]
-			if px < 0.5 {
-				px = 0.5
-			}
-			if py < 0.5 {
-				py = 0.5
-			}
-			if px > float32(width)-1.5 {
-				px = float32(width) - 1.5
-			}
-			if py > float32(height)-1.5 {
-				py = float32(height) - 1.5
-			}
-			i = (int)(px)
-			j = (int)(py)
-			fx = px - float32(i)
-			fy = py - float32(j)
-			s = source[j*stride+i:]
-			d[x] = (s[0]*(1-fx)+s[1]*fx)*(1-fy) +
-				(s[stride]*(1-fx)+s[stride+1]*fx)*fy
-		}
+	defer fd.Close()
+
+	data, err := os.Mmap(int(fd.Fd()), 0,
+		size, os.ProtRead|os.ProtWrite, os.MapShared)
+	if err != nil {
+		return err
+	}
+
+	pool, err := w.display.shm.CreatePool(fd.Fd(), int32(size))
+	if err != nil {
+		return err
+	}
+	buf, err := pool.CreateBuffer(0, int32(width), int32(height), int32(stride), format)
+	if err != nil {
+		return err
+	}
+
+	buffer.buffer = buf
+	// add buffer releaser here
+	wlclient.BufferAddListener(buffer.buffer, buffer)
+	pool.Destroy()
+
+	buffer.shmData = data
+
+	return nil
+}
+func (window *window) HandleSurfaceConfigure(ev zxdg.SurfaceConfigureEvent) {
+	window.SurfaceConfigure(window.xdgSurface, ev.Serial)
+}
+func (window *window) SurfaceConfigure(sf *zxdg.Surface, serial uint32) {
+
+	handle(sf.AckConfigure(serial))
+
+	if window.waitForConfigure {
+		redraw(window, nil, 0)
+		window.waitForConfigure = false
 	}
 
 }
 
-func project(smoke *smoke, time uint32, u []float32, v []float32, p []float32, div []float32, width int32, height int32) {
-	var x, y, k, l, s int
-	var h = 1.0 / float32(width)
+func (window *window) HandleToplevelConfigure(ev zxdg.ToplevelConfigureEvent) {
 
-	s = int(width)
-	for i := 0; i < int(height*width); i++ {
-		p[i] = 0.
-	}
-	for y = 1; y < int(height)-1; y++ {
-		l = y * s
-		for x = 1; x < int(width)-1; x++ {
-			div[l+x] = -0.5 * h * (u[l+x+1] - u[l+x-1] +
-				v[l+x+s] - v[l+x-s])
-			p[l+x] = 0
-		}
+}
+
+func (window *window) HandleToplevelClose(ev zxdg.ToplevelCloseEvent) {
+}
+
+func (d *display) HandleShmFormat(ev wl.ShmFormatEvent) {
+
+	if ev.Format == wl.ShmFormatXrgb8888 {
+		d.hasXrgb = true
 	}
 
-	for k = 0; k < 5; k++ {
-		for y = 1; y < int(height)-1; y++ {
-			l = y * s
-			for x = 1; x < int(width)-1; x++ {
-				p[l+x] = (div[l+x] +
-					p[l+x-1] +
-					p[l+x+1] +
-					p[l+x-s] +
-					p[l+x+s]) / 4
-			}
-		}
-	}
+}
 
-	for y = 1; y < int(height)-1; y++ {
-		l = y * s
-		for x = 1; x < int(width)-1; x++ {
-			u[l+x] -= 0.5 * (p[l+x+1] - p[l+x-1]) / h
-			v[l+x] -= 0.5 * (p[l+x+s] - p[l+x-s]) / h
-		}
+func (d *display) HandleWmBasePing(ev zxdg.WmBasePingEvent) {
+	d.shell.Pong(ev.Serial)
+}
+
+func (d *display) HandleRegistryGlobal(ev wl.RegistryGlobalEvent) {
+	d.RegistryGlobal(d.registry, ev.Name, ev.Interface, ev.Version)
+}
+
+func (d *display) RegistryGlobal(reg *wl.Registry, goid uint32, face string,
+	version uint32) {
+
+	goFace := face
+
+	switch goFace {
+	case "wl_compositor":
+		d.compositor = wlclient.RegistryBindCompositorInterface(d.registry, goid, 1)
+
+	case "xdg_wm_base":
+		d.shell = wlclient.RegistryBindWmBaseInterface(d.registry, goid, 1)
+
+		zxdg.WmBaseAddListener(d.shell, d)
+
+	case "zwp_fullscreen_shell_v1":
+
+	case "wl_shm":
+		d.shm = wlclient.RegistryBindShmInterface(d.registry, goid, 1)
+		wlclient.ShmAddListener(d.shm, d)
+
+	default:
+		log.Println("Other register global", goFace)
 	}
 }
 
-const maxx = 512
-const maxy = 256
+func (*display) HandleRegistryGlobalRemove(ev wl.RegistryGlobalRemoveEvent) {
 
-func (smoke *smoke) Resize(widget *window.Widget, _ int32, _ int32, width int32, height int32) {
+}
 
-	if smoke.smallwidth == width && smoke.smallheight == height {
-		return
+func createDisplay() *display {
+	disp := &display{
+		hasXrgb: false,
+	}
+	d, err := wlclient.DisplayConnect(nil)
+	if err != nil {
+		println("Could not connect to Wayland.")
+		handle(err)
 	}
 
-	size := int(width) * int(height)
+	disp.display = d
 
-	var olduv = smoke.bb[0].uv
-
-	smoke.bb[0].d = make([]float32, size)
-	smoke.bb[1].d = make([]float32, size)
-
-	if smoke.pipe {
-		<-olduv
-		smoke.bb[0].uu <- nil
-		smoke.bb[0].vv <- nil
-		<-olduv
+	reg, err := disp.display.GetRegistry()
+	if err != nil {
+		println("Could not get Registry.")
+		handle(err)
 	}
-	smoke.bb[0].uv = make(chan [2][]float32, 1)
-	if width > maxx {
-		smoke.smallwidth = width
-		smoke.width = maxx
+
+	disp.registry = reg
+
+	wlclient.RegistryAddListener(disp.registry, disp)
+	handle(wlclient.DisplayRoundtrip(disp.display))
+	if disp.shm == nil {
+		log.Fatal("No wl_shm global\n")
+	}
+
+	handle(wlclient.DisplayRoundtrip(disp.display))
+
+	if !disp.hasXrgb {
+		log.Fatal("WL_SHM_FORMAT_XRGB32 not available\n")
+	}
+
+	return disp
+}
+
+func createWindow(disp *display, width, height int) *window {
+
+	win := &window{
+		callback: nil,
+		display:  disp,
+		width:    width,
+		height:   height,
+	}
+
+	surf, err := disp.compositor.CreateSurface()
+	if err != nil {
+		println("cannot create surface")
+		handle(err)
+	}
+	win.surface = surf
+
+	if disp.shell != nil {
+		xdgSurf, err := disp.shell.GetSurface(win.surface)
+		if err != nil {
+			println("cannot get xdg surface")
+			handle(err)
+		}
+		win.xdgSurface = xdgSurf
+
+		win.xdgSurface.AddListener(win)
+
+		xdgToplevel, err := win.xdgSurface.GetToplevel()
+		if err != nil {
+			println("xdg toplevel is wrong")
+			handle(err)
+		}
+
+		win.xdgToplevel = xdgToplevel
+
+		zxdg.ToplevelAddListener(win.xdgToplevel, win)
+
+		handle(win.xdgToplevel.SetTitle("Title"))
+
+		handle(win.surface.Commit())
+		win.waitForConfigure = true
+
 	} else {
-		smoke.smallwidth = width
-		smoke.width = width
+		log.Fatal("Unknown shell\n")
 	}
-	if height > maxy {
-		smoke.smallheight = height
-		smoke.height = maxy
+
+	return win
+}
+func windowNextBuffer(window *window) *buffer {
+	var buffer *buffer
+
+	if !window.buffers[0].busy {
+		buffer = &window.buffers[0]
+	} else if !window.buffers[1].busy {
+		buffer = &window.buffers[1]
 	} else {
-		smoke.smallheight = height
-		smoke.height = height
+		log.Println("All Buffers Busy")
+		return nil
 	}
-	smoke.pipeline(smoke.width, smoke.height)
-	smoke.bb[0].uu <- make([]float32, size)
-	smoke.bb[0].vv <- make([]float32, size)
 
-	//smoke.widget.ScheduleResize(smoke.width, smoke.height)
-	smoke.pipe = true
-}
+	if buffer.buffer == nil {
+		err := createShmBuffer(window, buffer,
+			window.width, window.height,
+			wl.ShmFormatXrgb8888)
+		if err != nil {
+			log.Println(err)
+			return nil
+		}
 
-func render(smoke *smoke, surface cairo.Surface) {
-	var dst8 = surface.ImageSurfaceGetData()
-	var width = surface.ImageSurfaceGetWidth()
-	var height = surface.ImageSurfaceGetHeight()
-	var stride = surface.ImageSurfaceGetStride()
-
-	data := smoke.o
-
-	for y := 1; y < height-1; y++ {
-		var yy = (y * int(smoke.height) / int(smoke.smallheight)) * int(smoke.width)
-		for x := 1; x < width-1; x++ {
-			var xx = x * int(smoke.width) / int(smoke.smallwidth)
-			var c = uint32(data[xx+yy] * 800.)
-			if c > 255 {
-				c = 255
-			}
-			var a = c
-			if a < 0x33 {
-				a = 0x33
-			}
-			if dst8 != nil {
-				dst8[4*x+y*stride+0] = byte(c)
-				dst8[4*x+y*stride+1] = byte(c)
-				dst8[4*x+y*stride+2] = byte(c)
-				dst8[4*x+y*stride+3] = byte(a)
-			}
+		/* paint the padding */
+		for i := range buffer.shmData {
+			buffer.shmData[i] = 0xff
 		}
 	}
+
+	return buffer
 }
-func (smoke *smoke) pipeline(width int32, height int32) {
-	const lastTime = 600000
-	go func() {
-		for {
-			var u0 []float32
-			u0 = <-smoke.bb[0].uu
-			if u0 == nil {
-				smoke.bb[1].uu <- nil
-				return
-			}
-			u1 := make([]float32, len(u0))
-			diffuse(smoke, lastTime, u0, u1, width, height)
-			smoke.bb[1].uu <- u1
-		}
-	}()
-	go func() {
-		for {
-			var v0 []float32
-			v0 = <-smoke.bb[0].vv
-			if v0 == nil {
-				smoke.bb[1].vv <- nil
-				return
-			}
-			v1 := make([]float32, len(v0))
-			diffuse(smoke, lastTime, v0, v1, width, height)
-			smoke.bb[1].vv <- v1
-		}
-	}()
 
-	go func() {
-		for {
-			u1 := <-smoke.bb[1].uu
-			v1 := <-smoke.bb[1].vv
-			if u1 == nil || v1 == nil {
-				smoke.bb[0].uv <- [2][]float32{nil, nil}
-				return
-			}
-			u0 := make([]float32, len(u1))
-			v0 := make([]float32, len(v1))
-
-			project(smoke, lastTime, u1, v1, u0, v0, width, height)
-			advect(smoke, lastTime, u1, v1, u1, u0, width, height)
-			advect(smoke, lastTime, u1, v1, v1, v0, width, height)
-			project(smoke, lastTime, u0, v0, u1, v1, width, height)
-			smoke.bb[0].uv <- [2][]float32{u0, v0}
-		}
-	}()
-
-}
-func (smoke *smoke) Redraw(widget *window.Widget) {
-	var lastTime = smoke.widget.WidgetGetLastTime()
-
-	uv := <-smoke.bb[0].uv
-
-	if len(uv[0]) == len(smoke.bb[0].d) && len(uv[1]) == len(smoke.bb[1].d) {
-
-		diffuse(smoke, lastTime/30, smoke.bb[0].d, smoke.bb[1].d, smoke.width, smoke.height)
-		advect(smoke, lastTime/30, uv[0], uv[1], smoke.bb[1].d, smoke.bb[0].d, smoke.width, smoke.height)
-
-		smoke.o = smoke.bb[0].d
-
-	}
-	smoke.bb[0].uu <- uv[0]
-	smoke.bb[0].vv <- uv[1]
-
-	var surface = smoke.window.WindowGetSurface()
-
-	if surface != nil {
-
-		render(smoke, surface)
-		surface.Destroy()
-	}
-
-	smoke.widget.ScheduleRedraw()
-}
-func smokeMotionHandler(smoke *smoke, x float32, y float32) {
-
-	dx := smoke.lx - x
-	dy := smoke.ly - y
-
-	dt := 0.8 / (dx*dx + dy*dy + 1.0)
-
-	if dt > 1. {
-		dt = 1.
-	}
-
-	smoke.lx = x
-	smoke.ly = y
-
-	x *= float32(smoke.width) / float32(smoke.smallwidth)
-	y *= float32(smoke.height) / float32(smoke.smallheight)
-	var i0, i1, j0, j1 float32
-	var k, i, j int
-	var d float32 = 5
-
-	if x-d < 1 {
-		i0 = 1
+func (window *window) paintPixels(time uint32, buf *buffer) {
+	var or int
+	halfw, halfh := window.width/2, window.height/2
+	if halfw < halfh {
+		or = halfw - 8
 	} else {
-		i0 = x - d
+		or = halfh - 8
 	}
-	if i0+2.*d > float32(smoke.width)-1. {
-		i1 = float32(smoke.width) - 1.
-	} else {
-		i1 = i0 + 2.*d
-	}
-	if y-d < 1 {
-		j0 = 1
-	} else {
-		j0 = y - d
-	}
-	if j0+2*d > float32(smoke.height)-1 {
-		j1 = float32(smoke.height) - 1
-	} else {
-		j1 = j0 + 2*d
-	}
+	ir := or - 32
+	or *= or
+	ir *= ir
 
-	for i = int(i0); i < int(i1); i++ {
-		for j = int(j0); j < int(j1); j++ {
-			k = j*int(smoke.width) + i
+	var iter = 0
 
-			smoke.bb[0].d[k] += dt
-		}
-	}
-
-	var freq = (smoke.smallwidth * smoke.smallheight) / (smoke.width * smoke.height)
-
-	if rand.Int()%int(2*freq+1) == 0 {
-		uv := <-smoke.bb[0].uv
-
-		for i = int(i0); i < int(i1); i++ {
-			for j = int(j0); j < int(j1); j++ {
-				k = j*int(smoke.width) + i
-
-				uv[0][k] += float32(1024 - (rand.Int() & 2047))
-				uv[1][k] += float32(1024 - (rand.Int() & 2047))
+	for y := 0; y < window.height; y++ {
+		y2 := (y - halfh) * (y - halfh)
+		for x := 0; x < window.width; x++ {
+			var v int
+			r2 := (x-halfw)*(x-halfw) + y2
+			if r2 < ir {
+				v = (r2/32 + int(time)/64) * 0x0080401
+			} else if r2 < or {
+				v = (y + int(time)/32) * 0x0080401
+			} else {
+				v = (x + int(time)/16) * 0x0080401
 			}
+			a := 0
+			r := v >> 16
+			g := v >> 8
+			b := v
+			buf.shmData[iter] = byte(b)
+			iter++
+			buf.shmData[iter] = byte(g)
+			iter++
+			buf.shmData[iter] = byte(r)
+			iter++
+			buf.shmData[iter] = byte(a)
+			iter++
 		}
-
-		smoke.bb[0].uu <- uv[0]
-		smoke.bb[0].vv <- uv[1]
 	}
 }
-func (smoke *smoke) Key(
-	window *window.Window,
-	input *window.Input,
-	time uint32,
-	key uint32,
-	notUnicode uint32,
-	state wl.KeyboardKeyState,
-	data window.WidgetHandler,
-) {
-	println(notUnicode)
+func (window *window) HandleCallbackDone(ev wl.CallbackDoneEvent) {
+	window.CallbackDone(ev.C, ev.CallbackData)
 }
-func (*smoke) Focus(window *window.Window, device *window.Input) {
-
-}
-func (*smoke) Enter(widget *window.Widget, input *window.Input, x float32, y float32) {
-}
-func (*smoke) Leave(widget *window.Widget, input *window.Input) {
+func (window *window) CallbackDone(callback *wl.Callback, time uint32) {
+	redraw(window, callback, time)
 }
 
-func (smoke *smoke) Motion(
-	widget *window.Widget,
-	input *window.Input,
-	time uint32,
-	x float32,
-	y float32,
-) int {
-	smokeMotionHandler(smoke, x, y)
+func redraw(win *window, callback *wl.Callback, time uint32) {
 
-	return window.CursorHand1
-}
+	buffer := windowNextBuffer(win)
 
-func (*smoke) Button(
-	widget *window.Widget,
-	input *window.Input,
-	time uint32,
-	button uint32,
-	state wl.PointerButtonState,
-	data window.WidgetHandler,
-) {
-}
+	win.paintPixels(time, buffer)
+	handle(win.surface.Attach(buffer.buffer, 0, 0))
+	handle(win.surface.Damage(0, 0,
+		int32(win.width), int32(win.height)))
 
-func (*smoke) TouchUp(
-	widget *window.Widget,
-	input *window.Input,
-	serial uint32,
-	time uint32,
-	id int32,
-) {
-}
-
-func (*smoke) TouchDown(
-	widget *window.Widget,
-	input *window.Input,
-	serial uint32,
-	time uint32,
-	id int32,
-	x float32,
-	y float32,
-) {
-}
-
-func (smoke *smoke) TouchMotion(
-	widget *window.Widget,
-	input *window.Input,
-	time uint32,
-	id int32,
-	x float32,
-	y float32,
-) {
-
-	smokeMotionHandler(smoke, x, y)
-
-}
-func (*smoke) TouchFrame(widget *window.Widget, input *window.Input) {
-}
-func (*smoke) TouchCancel(widget *window.Widget, width int32, height int32) {
-}
-
-func (*smoke) Axis(
-	widget *window.Widget,
-	input *window.Input,
-	time uint32,
-	axis uint32,
-	value float32,
-) {
-}
-func (*smoke) AxisSource(widget *window.Widget, input *window.Input, source uint32) {
-}
-func (*smoke) AxisStop(widget *window.Widget, input *window.Input, time uint32, axis uint32) {
-}
-
-func (*smoke) AxisDiscrete(
-	widget *window.Widget,
-	input *window.Input,
-	axis uint32,
-	discrete int32,
-) {
-}
-func (*smoke) PointerFrame(widget *window.Widget, input *window.Input) {
-}
-
-func (smoke *smoke) free() {
-	// tear down the rendering pipe
-	var olduv = smoke.bb[0].uv
-	if smoke.pipe {
-		<-olduv
-		smoke.bb[0].uu <- nil
-		smoke.bb[0].vv <- nil
-		<-olduv
+	if callback != nil {
+		wlclient.CallbackDestroy(callback)
 	}
 
-	smoke.bb[0].d = nil
-	smoke.bb[0].uv = nil
-	smoke.bb[1].d = nil
-	smoke.bb[1].uu = nil
-	smoke.bb[1].vv = nil
-	smoke.bb[0].uu = nil
-	smoke.bb[0].vv = nil
+	cb, err := win.surface.Frame()
+	handle(err)
+	win.callback = cb
+
+	wlclient.CallbackAddListener(win.callback, win)
+	handle(win.surface.Commit())
+	buffer.busy = true
 }
 
 func main() {
 
-	var smoke smoke
-
-	d, err := window.DisplayCreate([]string{})
-	if err != nil {
-		fmt.Println(err)
+	display := createDisplay()
+	window := createWindow(display, 250, 250)
+	if window == nil {
 		return
 	}
 
-	smoke.width = 200
-	smoke.height = 200
-	smoke.display = d
-	smoke.window = window.Create(d)
+	// Initialise damage to full surface, so the padding gets painted
+	handle(window.surface.Damage(0, 0, int32(window.width), int32(window.height)))
 
-	smoke.widget = smoke.window.AddWidget(&smoke)
+	if !window.waitForConfigure {
+		redraw(window, nil, 0)
+	}
 
-	smoke.window.SetTitle("smoke")
-	smoke.window.SetBufferType(window.BufferTypeShm)
-	smoke.window.SetKeyboardHandler(&smoke)
-	rand.Seed(int64(time.Now().Nanosecond()))
+	for wlclient.DisplayDispatch(display.display) == nil {
+	}
 
-	var size = int(smoke.height * smoke.width)
-
-	smoke.bb[0].d = make([]float32, size)
-	smoke.bb[0].uv = make(chan [2][]float32, 1)
-	smoke.bb[1].d = make([]float32, size)
-	smoke.bb[1].uu = make(chan []float32, 1)
-	smoke.bb[1].vv = make(chan []float32, 1)
-	smoke.bb[0].uu = make(chan []float32, 1)
-	smoke.bb[0].vv = make(chan []float32, 1)
-
-	//smoke.bb[0].uv <- [2][]float32{make([]float32, size), make([]float32, size)}
-
-	smoke.widget.Userdata = &smoke
-
-	smoke.widget.ScheduleResize(smoke.width, smoke.height)
-
-	window.DisplayRun(d)
-
-	smoke.widget.Destroy()
-	smoke.window.Destroy()
-	d.Destroy()
+	print("go-wayland-simpleshm exiting\n")
 
 }
