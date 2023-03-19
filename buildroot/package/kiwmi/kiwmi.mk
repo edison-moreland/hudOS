082b5ba417f56f0686496f3fb0e8e4a2628fcff6
Edison Moreland <edison@devdude.net>
2023-03-18 19:24:50 -0700

    kiwmi?

---
+++ buildroot/package/kiwmi/kiwmi.mk
@@ -0,0 +1,7 @@
+KIWMI_VERSION = 17814972abe6a8811a586fa87c99a2b16a86075f
+KIWMI_SITE = https://github.com/buffet/kiwmi.git
+KIWMI_SITE_METHOD = git
+KIWMI_INSTALL_TARGET = YES
+KIWMI_DEPENDENCIES = host-clang wlroots lua pixman
+
+$(eval $(meson-package))
