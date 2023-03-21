a0013092ec69425ac4b3d0cabde5a67cf5aadb9e
Edison Moreland <edison@devdude.net>
2023-03-20 22:39:32 -0700

    Start apps, restart compositor, when glasses connect

--- app_builder/steps/generate_service.sh
+++ app_builder/steps/generate_service.sh
@@ -3,12 +3,27 @@ set -euo pipefail
 
 STEP_CONFIG="$1"
 OUT="$(echo "${STEP_CONFIG}" | jq -r '.out')"
+ON_GLASSES="$(echo "${STEP_CONFIG}" | jq -r '.onGlasses // false')"
+
 
 cat <<EOF | envsubst >"${OUT}"
 [Unit]
 Description=${APP_NAME}, a HUD Application
 AssertPathExists=/opt/hud/run/wayland-0
+EOF
+
+if [[ "${ON_GLASSES}" == "true" ]]; then
+cat <<EOF | envsubst >>"${OUT}"
+PartOf=hud-glasses.target
+EOF
+else
+cat <<EOF | envsubst >>"${OUT}"
 PartOf=hud-apps.target
+EOF
+fi
+
+
+cat <<EOF | envsubst >>"${OUT}"
 
 [Service]
 User=hud
@@ -21,6 +36,17 @@ Environment=XDG_RUNTIME_DIR=/opt/hud/run
 ExecStart=/usr/bin/${APP_NAME}
 Restart=always
 
+EOF
+
+if [[ "${ON_GLASSES}" == "true" ]]; then
+cat <<EOF | envsubst >>"${OUT}"
+[Install]
+WantedBy=hud-glasses.target
+EOF
+else
+cat <<EOF | envsubst >>"${OUT}"
 [Install]
 WantedBy=hud-apps.target
 EOF
+fi
+
--- buildroot/package/hudOS/hudOS.mk
+++ buildroot/package/hudOS/hudOS.mk
@@ -12,6 +12,10 @@ define HUDOS_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/etc/systemd/system-preset/50-disable_getty.preset; \
 	$(INSTALL) -D -m 644 $(HUDOS_PKGDIR)/fw_env.config \
 		$(TARGET_DIR)/etc/fw_env.config; \
+	$(INSTALL) -D -m 644 $(HUDOS_PKGDIR)/95-nreal-glasses.rules \
+		$(TARGET_DIR)/etc/udev/rules.d/95-nreal-glasses.rules; \
+	$(INSTALL) -D -m 644 $(HUDOS_PKGDIR)/nreal-glasses.target \
+		$(TARGET_DIR)/etc/systemd/system/nreal-glasses.target; \
 	mkdir -p $(TARGET_DIR)/opt/hud/systemd/system/; \
 	mkdir -p $(TARGET_DIR)/usr/share/icons/;
 endef
