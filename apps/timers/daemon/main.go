77f79e31fcb8a548982a766c4242a61823b9dff6
Edison Moreland <edison@devdude.net>
2023-03-25 19:43:23 -0700

    Start protobuf service

--- apps/timers/daemon/main.go
+++ apps/timers/daemon/main.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/rs/zerolog"
+
+	"github.com/edison-moreland/nreal-hud/go-sdk/app/service"
+	"github.com/edison-moreland/nreal-hud/proto/timers"
+)
 
 // Daemon listens for protobuf messages, and manages the timer windows
-
 func main() {
-	fmt.Printf("hello world!")
+	service.ServiceGRPC("timers", &timers.Timers_ServiceDesc, func(logger zerolog.Logger) (interface{}, error) {
+		return &TimersService{
+			Logger: logger,
+		}, nil
+	})
+}
+
+type TimersService struct {
+	timers.UnimplementedTimersServer
+	zerolog.Logger
+}
+
+func (t *TimersService) NewTimer(ctx context.Context, request *timers.NewTimerRequest) (*timers.NewTimerResponse, error) {
+	t.Info().Msg("New timer!!!")
+
+	return &timers.NewTimerResponse{}, nil
+}
+
+func (t *TimersService) StopTimer(ctx context.Context, request *timers.StopTimerRequest) (*timers.StopTimerResponse, error) {
+	t.Info().Msg("Stop timer!!!")
+
+	return &timers.StopTimerResponse{}, nil
+}
+
+func (t *TimersService) ListTimers(ctx context.Context, request *timers.ListTimersRequest) (*timers.ListTimersResponse, error) {
+	t.Info().Msg("List timers!!!")
+
+	return &timers.ListTimersResponse{}, nil
 }
